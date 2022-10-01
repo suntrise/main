e3d857efc86b076e1134817ba33d225aacfd2dc9
suntrise <89229642+suntrise@users.noreply.github.com>
2022-10-01 16:42:10 +0800

    Update ggc.js

--- ggc.js
+++ ggc.js
@@ -3,7 +3,10 @@ var arr=[]
 var i=0
 var str=""
 	function ranggc() {
-		arr = ["<h1>Start me up</h1><h3>（Windows 95 广告语）</h3>","<h1>Everything at once</h1><h3>（Windows 8 广告语）</h3>","<h1>Your world on the go</h1><h3>（Windows 8 广告语）</h3>"]
+		arr = ["<h1>Start me up</h1><h3>（Windows 95 广告语）</h3>",
+		"<h1>Everything at once（一切精彩尽此时）</h1><h3>（Windows 8 广告语）</h3>",
+		"<h1>Your world on the go（你的世界，随你走）</h1><h3>（Windows 8 广告语）</h3>",
+		"<h1>The Wow Starts Now</h1><h3>（Windows Vista 广告语）</h3>"]
 		var i = Math.round(Math.random()*(arr.length-1))
 		str = arr[i]
 		ggc.innerHTML = str;
