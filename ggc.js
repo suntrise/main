9c36e4a6a4f5a142bbd45cd04ce6d336f8041127
suntrise <89229642+suntrise@users.noreply.github.com>
2022-10-01 17:14:55 +0800

    Update ggc.js

--- ggc.js
+++ ggc.js
@@ -6,9 +6,9 @@ var str=""
 		arr = ["<h1>Start me up.</h1><h3>（Windows 95 广告语）</h3>",
 		"<h1>Yes you can.</h1><h3>（Windows XP 广告语）</h3>",
 		"<h1>The Wow Starts Now.</h1><h3>（Windows Vista 广告语）</h3>",
-		"<h1>Your PC, Simplified.（电脑，就这么简单）</h1><h3>（Windows 7 广告语）</h3>",
-		"<h1>Everything at once.</h1><h3>（Windows 8 广告语）</h3>",
-		"<h1>Your world on the go.</h1><h3>（Windows 8 广告语）</h3>"]
+		"Your PC, Simplified.（电脑，就这么简单）<h3>（Windows 7 广告语）</h3>",
+		"<h1>Everything at once.（一切精彩尽此时）</h1><h3>（Windows 8 广告语）</h3>",
+		"<h1>Your world on the go.（你的世界，随你走）</h1><h3>（Windows 8 广告语）</h3>"]
 		i = Math.round(Math.random()*(arr.length-1))
 		str = arr[i]
 		ggc.innerHTML = str;
