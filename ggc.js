8d07ee56edc0a75b548541ee6cc59a001e2cc8b0
suntrise <89229642+suntrise@users.noreply.github.com>
2022-10-02 11:37:13 +0800

    Update ggc.js

--- ggc.js
+++ ggc.js
@@ -8,7 +8,9 @@ var str=""
 		"<h1>The Wow Starts Now.</h1><h3>（Windows Vista 广告语）</h3>",
 		"<h1>Your PC, Simplified.（电脑，就这么简单）</h1><h3>（Windows 7 广告语）</h3>",
 		"<h1>Everything at once.（一切精彩尽此时）</h1><h3>（Windows 8 广告语）</h3>",
-		"<h1>Your World On The Go.（你的世界，随你走）</h1><h3>（Windows 8 广告语）</h3>",
+		"<h1>Your world on the go.（你的世界，随你走）</h1><h3>（Windows 8 广告语）</h3>",
+		"<h1>Work. Play. Together.</h1><h3>（Windows 8 广告语）</h3>",
+		"<h1>Life without walls.</h1><h3>（Windows 8 广告语）</h3>",
     	"<h1>坐和放宽</h1>"]
 		i = Math.round(Math.random()*(arr.length-1))
 		str = arr[i]
