877faf58a98dbff74ca2a32160c20549182fc7e1
suntrise <89229642+suntrise@users.noreply.github.com>
2022-11-12 18:27:20 +0800

    Update ggc.js

--- ggc.js
+++ ggc.js
@@ -7,7 +7,6 @@ var str=""
 		"<h1>Yes you can.</h1><h3>—— Windows XP 广告语</h3>",
     "<h1>Do more with less.</h1><h3>—— Windows Server 2003 广告语</h3>",
 		"<h1>The Wow Starts Now.</h1><h3>—— Windows Vista 广告语</h3>",
-    "<h1>Virtualize Responsibility.</h1><h3>—— Windows Server 2008 广告语</h3>",
 		"<h1>Your PC, Simplified.（电脑，就这么简单）</h1><h3>—— Windows 7 广告语</h3>",
 		"<h1>Everything at once.（一切精彩尽此时）</h1><h3>—— Windows 8 广告语）</h3>",
 		"<h1>Your World On the Go.（你的世界，随你走）</h1><h3>—— Windows 8 广告语</h3>",
