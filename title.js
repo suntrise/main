1e8f94437d36ea212f4cc5fefdae3899b03451b6
suntrise <89229642+suntrise@users.noreply.github.com>
2022-11-12 19:19:42 +0800

    Update title.js

--- title.js
+++ title.js
@@ -1,11 +1,14 @@
-var xhr = new XMLHttpRequest();
+  var xhr = new XMLHttpRequest();
   xhr.open('get', 'https://v1.hitokoto.cn');
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4) {
       var data = JSON.parse(xhr.responseText);
       var hitokoto = document.getElementById('hitokoto_text');
       hitokoto.href = 'https://hitokoto.cn/?uuid=' + data.uuid
-      hitokoto.innerText = data.hitokoto;
+      if(data.from_who == null){
+        data.from_who = "";
+      }
+      hitokoto.innerText = data.hitokoto +"—— " +data.from_who +"「" +data.from +"」";
     }
   }
   xhr.send();
